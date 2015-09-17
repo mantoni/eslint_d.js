2f70fc0c2c7edd35ea71898a04c0040421d49d93
Maximilian Antoni <mail@maxantoni.de>
2015-09-18 00:56:39 +0200

    Initial implementation

---
+++ bin/eslint_d.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+'use strict';
+
+var net = require('net');
+var fs = require('fs');
+var eslint = require('eslint');
+
+var engine = new eslint.CLIEngine();
+var formatter = engine.getFormatter('compact');
+
+var server = net.createServer({
+  allowHalfOpen: true
+}, function (con) {
+  var data = '';
+  con.on('data', function (chunk) {
+    data += chunk;
+  });
+  con.on('end', function () {
+    var report = engine.executeOnFiles([data]);
+    con.write(formatter(report.results));
+    con.end();
+  });
+});
+
+var portFile = process.env.HOME + '/.eslint_d_port';
+server.listen(function () {
+  var port = server.address().port;
+  fs.writeFileSync(portFile, String(port));
+});
+
+process.on('exit', function () {
+  fs.unlinkSync(portFile);
+});
+process.on('SIGINT', function () {
+  process.exit();
+});
