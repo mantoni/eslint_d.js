ab0ecbb790106d0defb94ae1fa798fa30627739a
Maximilian Antoni <mail@maxantoni.de>
2015-09-18 22:45:51 +0200

    Control the server via a client

--- bin/eslint_d.js
+++ bin/eslint_d.js
@@ -1,53 +1,29 @@
 #!/usr/bin/env node
 'use strict';
 
-var net = require('net');
-var fs = require('fs');
-var eslint = require('eslint');
-
-var engines = {};
-
-function getEngine(cwd) {
-  var engine = engines[cwd];
-  if (!engine) {
-    engine = new eslint.CLIEngine();
-    engines[cwd] = engine;
-  }
-  return engine;
+function start() {
+  require('../lib/launcher')();
 }
 
+var cmd = process.argv[2];
+if (cmd === 'start') {
 
-var server = net.createServer({
-  allowHalfOpen: true
-}, function (con) {
-  var data = '';
-  con.on('data', function (chunk) {
-    data += chunk;
-  });
-  con.on('end', function () {
-    var parts = data.split(' ');
-    var cwd = parts[0];
-    process.chdir(cwd);
-    var engine = getEngine(cwd);
-    var report = engine.executeOnFiles(parts.slice(1));
-    var formatter = engine.getFormatter('compact');
-    con.write(formatter(report.results));
-    con.end();
-  });
-});
+  start();
 
-var portFile = process.env.HOME + '/.eslint_d_port';
-server.listen(function () {
-  var port = server.address().port;
-  fs.writeFileSync(portFile, String(port));
-});
+} else {
 
-process.on('exit', function () {
-  fs.unlinkSync(portFile);
-});
-process.on('SIGTERM', function () {
-  process.exit();
-});
-process.on('SIGINT', function () {
-  process.exit();
-});
+  var commands = ['stop', 'status', 'restart', 'lint'];
+  if (commands.indexOf(cmd) === -1) {
+    process.stdout.write('Usage: eslint_d [start|stop|status|restart|lint]\n');
+    return;
+  }
+
+  var client = require('../lib/client');
+  if (cmd === 'restart') {
+    client.stop(function () {
+      process.nextTick(start);
+    });
+  } else {
+    client[cmd](process.argv.slice(3));
+  }
+}
