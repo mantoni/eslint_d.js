25dddb05ee49b27509debfc9867191a1d25135a3
Maximilian Antoni <mail@maxantoni.de>
2018-12-01 19:01:02 +0100

    Run tests against eslint v4.0 and v5.0

--- test/linter-test.js
+++ test/linter-test.js
@@ -1,6 +1,7 @@
 /*eslint-env mocha*/
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
 const resolve = require('resolve');
 const { assert, refute, sinon } = require('@sinonjs/referee-sinon');
@@ -116,147 +117,176 @@ describe('linter', () => {
 
   });
 
-  describe('single file', () => {
+  const fixture_fail = `${cwd}/test/fixture/fail.txt`;
+  const fixture_warn = `${cwd}/test/fixture/warn.txt`;
+  const lib_linter = `${cwd}/lib/linter.js`;
 
-    it('succeeds on lib/linter.js', () => {
-      const out = linter.lint(cwd, ['lib/linter.js']);
+  function withinDirectory(eslint_version, cwd) {
 
-      assert.equals(out, '');
+    describe(eslint_version, () => {
+
+      it('has expected eslint version', () => {
+        const path = `${cwd}/node_modules/eslint/package.json`;
+        // eslint-disable-next-line no-sync
+        const { version } = JSON.parse(fs.readFileSync(path, 'utf8'));
+
+        const match = version.match(/^([0-9]+)\.([0-9]+)\.([0-9]+)$/);
+
+        refute.isNull(match);
+        const parts = eslint_version.split('.');
+        for (let i = 0; i < parts.length; i++) {
+          assert.equals(match[i + 1], parts[i]);
+        }
+      });
+
+      describe('single file', () => {
+
+        it('succeeds on lib/linter.js', () => {
+          const out = linter.lint(cwd, [lib_linter]);
+
+          assert.equals(out, '');
+        });
+
+        it('fails on test/fixture/fail.txt', () => {
+          const out = linter.lint(cwd, [fixture_fail, '-f', 'unix']);
+
+          const space = out.indexOf(' ');
+          const slash = out.lastIndexOf('/', space);
+          const newline = out.indexOf('\n');
+
+          assert.equals(out.substring(slash, space), '/fail.txt:3:13:');
+          assert.equals(out.substring(space + 1, newline),
+            'Strings must use singlequote. [Error/quotes]');
+        });
+
+        it('adds `# exit 1` on failure', () => {
+          const out = linter.lint(cwd, [fixture_fail, '-f', 'unix']);
+
+          assert.equals(out.split('\n').pop(), '# exit 1');
+        });
+
+      });
+
+      describe('--stdin', () => {
+
+        it('runs on --stdin text', () => {
+          const out = linter.lint(cwd, ['--stdin', '-f', 'unix'],
+            'console.log();');
+
+          assert.equals(out.split('\n').shift(), '<text>:1:1: Use the global '
+            + 'form of \'use strict\'. [Error/strict]');
+        });
+
+        it('adds `# exit 1` on failure', () => {
+          const out = linter.lint(cwd, ['--stdin'], 'console.log();');
+
+          assert.equals(out.split('\n').pop(), '# exit 1');
+        });
+
+      });
+
+      describe('--fix-to-stdout', () => {
+
+        it('returns fixed script', () => {
+          const out = linter.lint(cwd, ['--stdin', '--fix-to-stdout'],
+            'console.log( "!" )\n');
+
+          assert.equals(out, 'console.log(\'!\');\n');
+        });
+
+        it('fails if --stdin is not given', () => {
+          const out = linter.lint(cwd, ['--fix-to-stdout', '.']);
+
+          assert.equals(out,
+            'The --fix-to-stdout option must be used with --stdin.\n# exit 1');
+        });
+
+        it('returns input if nothing to fix', () => {
+          const out = linter.lint(cwd, ['--stdin', '--fix-to-stdout'],
+            'console.log(\'!\');\n');
+
+          assert.equals(out, 'console.log(\'!\');\n');
+        });
+
+      });
+
+      describe('--print-config', () => {
+
+        it('fails with --stdin', () => {
+          const out = linter.lint(cwd, ['--stdin', '--print-config'],
+            'console.log( "!" )\n');
+
+          assert.equals(out, 'The --print-config option requires a single file '
+            + 'as positional argument.\n# exit 1');
+        });
+
+        it('fails with --stdin and positional argument', () => {
+          const out = linter.lint(cwd, ['--stdin', '--print-config', '.'],
+            'console.log( "!" )\n');
+
+          assert.equals(out, 'The --print-config option is not available for '
+            + 'piped-in code.\n# exit 1');
+        });
+
+      });
+
+      describe('--quiet', () => {
+
+        it('prints warnings by default', () => {
+          const out = linter.lint(cwd, [fixture_warn, '-f', 'unix']);
+
+          refute.equals(out, ''); // verify warn.txt prints warnings
+          assert.match(out, 'Warning');
+          refute.match(out, '# exit 1');
+        });
+
+        it('does not print warnings', () => {
+          const out = linter.lint(cwd, ['--quiet', fixture_warn]);
+
+          assert.equals(out, '');
+        });
+
+      });
+
+      describe('--max-warnings', () => {
+
+        it('adds `# exit 1` on failure', () => {
+          const out = linter.lint(cwd, [fixture_warn, '--max-warnings', '0']);
+
+          assert.equals(out.split('\n').pop(), '# exit 1');
+        });
+
+        it('does not add `# exit 1` if not exceeded', () => {
+          const out = linter.lint(cwd, [fixture_warn, '--max-warnings', '1']);
+
+          refute.equals(out.split('\n').pop(), '# exit 1');
+        });
+
+      });
+
+      describe('--color', () => {
+
+        it('enables color by default', () => {
+          linter.lint(cwd, ['--stdin'], '\'use strict\';');
+
+          assert.isTrue(linter.cache.get(cwd).chalk.enabled);
+        });
+
+        it('disables color if --no-color is passed', () => {
+          linter.lint(cwd, ['--stdin', '--no-color'], '\'use strict\';');
+
+          assert.isFalse(linter.cache.get(cwd).chalk.enabled);
+        });
+
+      });
+
     });
 
-    it('fails on test/fixture/fail.txt', () => {
-      const out = linter.lint(cwd, ['test/fixture/fail.txt', '-f', 'unix']);
+  }
 
-      const space = out.indexOf(' ');
-      const slash = out.lastIndexOf('/', space);
-      const newline = out.indexOf('\n');
-
-      assert.equals(out.substring(slash, space), '/fail.txt:3:13:');
-      assert.equals(out.substring(space + 1, newline),
-        'Strings must use singlequote. [Error/quotes]');
-    });
-
-    it('adds `# exit 1` on failure', () => {
-      const out = linter.lint(cwd, ['test/fixture/fail.txt', '-f', 'unix']);
-
-      assert.equals(out.split('\n').pop(), '# exit 1');
-    });
-
-  });
-
-  describe('--stdin', () => {
-
-    it('runs on --stdin text', () => {
-      const out = linter.lint(cwd, ['--stdin', '-f', 'unix'], 'console.log();');
-
-      assert.equals(out.split('\n').shift(),
-        '<text>:1:1: Use the global form of \'use strict\'. [Error/strict]');
-    });
-
-    it('adds `# exit 1` on failure', () => {
-      const out = linter.lint(cwd, ['--stdin'], 'console.log();');
-
-      assert.equals(out.split('\n').pop(), '# exit 1');
-    });
-
-  });
-
-  describe('--fix-to-stdout', () => {
-
-    it('returns fixed script', () => {
-      const out = linter.lint(cwd, ['--stdin', '--fix-to-stdout'],
-        'console.log( "!" )\n');
-
-      assert.equals(out, 'console.log(\'!\');\n');
-    });
-
-    it('fails if --stdin is not given', () => {
-      const out = linter.lint(cwd, ['--fix-to-stdout', '.']);
-
-      assert.equals(out,
-        'The --fix-to-stdout option must be used with --stdin.\n# exit 1');
-    });
-
-    it('returns input if nothing to fix', () => {
-      const out = linter.lint(cwd, ['--stdin', '--fix-to-stdout'],
-        'console.log(\'!\');\n');
-
-      assert.equals(out, 'console.log(\'!\');\n');
-    });
-
-  });
-
-  describe('--print-config', () => {
-
-    it('fails with --stdin', () => {
-      const out = linter.lint(cwd, ['--stdin', '--print-config'],
-        'console.log( "!" )\n');
-
-      assert.equals(out, 'The --print-config option requires a single file '
-        + 'as positional argument.\n# exit 1');
-    });
-
-    it('fails with --stdin and positional argument', () => {
-      const out = linter.lint(cwd, ['--stdin', '--print-config', '.'],
-        'console.log( "!" )\n');
-
-      assert.equals(out, 'The --print-config option is not available for '
-        + 'piped-in code.\n# exit 1');
-    });
-
-  });
-
-  describe('--quiet', () => {
-
-    it('prints warnings by default', () => {
-      const out = linter.lint(cwd, ['test/fixture/warn.txt', '-f', 'unix']);
-
-      refute.equals(out, ''); // verify warn.txt prints warnings
-      assert.match(out, 'Warning');
-      refute.match(out, '# exit 1');
-    });
-
-    it('does not print warnings', () => {
-      const out = linter.lint(cwd, ['--quiet', 'test/fixture/warn.txt']);
-
-      assert.equals(out, '');
-    });
-
-  });
-
-  describe('--max-warnings', () => {
-
-    it('adds `# exit 1` on failure', () => {
-      const out = linter.lint(cwd, ['test/fixture/warn.txt',
-        '--max-warnings', '0']);
-
-      assert.equals(out.split('\n').pop(), '# exit 1');
-    });
-
-    it('does not add `# exit 1` if not exceeded', () => {
-      const out = linter.lint(cwd, ['test/fixture/warn.txt',
-        '--max-warnings', '1']);
-
-      refute.equals(out.split('\n').pop(), '# exit 1');
-    });
-
-  });
-
-  describe('--color', () => {
-
-    it('enables color by default', () => {
-      linter.lint(cwd, ['--stdin'], '\'use strict\';');
-
-      assert.isTrue(linter.cache.get(cwd).chalk.enabled);
-    });
-
-    it('disables color if --no-color is passed', () => {
-      linter.lint(cwd, ['--stdin', '--no-color'], '\'use strict\';');
-
-      assert.isFalse(linter.cache.get(cwd).chalk.enabled);
-    });
-
-  });
+  withinDirectory('5', cwd);
+  withinDirectory('5.0', 'test/fixture/v5.0.x');
+  withinDirectory('4.0', 'test/fixture/v4.0.x');
 
   it('lets eslint handle unknown formatter', () => {
     assert.exception(() => {
