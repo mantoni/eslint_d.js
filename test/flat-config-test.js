399b46268db1c3deaae26ffb2935336082c8e84a
Amrit Kahlon <amritkahlon00@gmail.com>
2023-11-12 03:14:32 -0800

    feat: adapt to new eslint flat config (#277)

---
+++ test/flat-config-test.js
@@ -0,0 +1,58 @@
+/*eslint-env mocha*/
+'use strict';
+
+const { assert } = require('@sinonjs/referee-sinon');
+const api = require('eslint');
+const unsupported = require('../node_modules/eslint/lib/unsupported-api');
+const caches = require('../lib/caches');
+const linter = require('../lib/linter');
+
+describe('flat-config', () => {
+
+  beforeEach(() => {
+    caches.lru_cache.clear();
+    delete process.env.ESLINT_USE_FLAT_CONFIG;
+  });
+  afterEach(() => {
+    caches.lru_cache.clear();
+    delete process.env.ESLINT_USE_FLAT_CONFIG;
+  });
+
+  describe('without flat config', () => {
+    it('resolves ESLint class', async () => {
+      const cache = await caches.getCache(process.cwd());
+
+      assert.equals(typeof cache.eslint.ESLint, typeof api.ESLint);
+      assert.isUndefined(cache.eslint.FlatESLint);
+    });
+
+    it('runs lint with ESLint class', async () => {
+      await linter.invoke(process.cwd(), ['.'], undefined, () => null);
+    });
+  });
+
+  describe('with flat configV', () => {
+    it('resolves FlatESLint class', async () => {
+      process.env.ESLINT_USE_FLAT_CONFIG = 'true';
+
+      const cache = await caches.getCache(process.cwd());
+
+      assert.equals(
+        typeof cache.eslint.FlatESLint,
+        typeof unsupported.FlatESLint
+      );
+      assert.isUndefined(cache.eslint.ESLint);
+    });
+
+    it('runs lint with FlatESLint class', async () => {
+      process.env.ESLINT_USE_FLAT_CONFIG = 'true';
+
+      await linter.invoke(
+        process.cwd(),
+        ['-c', `${process.cwd()}/test/fixture/eslint.config.js`],
+        undefined,
+        () => null
+      );
+    });
+  });
+});
