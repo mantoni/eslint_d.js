0c63d32281c22cf66f8376d920fec5b13a44fe08
Damien Cassou <damien@cassou.me>
2024-08-01 07:51:01 +0200

    Extract list of supported eslint versions in test.integration.js

--- test/test.integration.js
+++ test/test.integration.js
@@ -4,6 +4,15 @@ import fs from 'node:fs/promises';
 import { createRequire } from 'node:module';
 import { assert, refute } from '@sinonjs/referee-sinon';
 
+const SUPPORTED_ESLINT_VERSIONS = [
+  'v4.0.x',
+  'v5.0.x',
+  'v6.0.x',
+  'v7.0.x',
+  'v8.0.x',
+  'v9.0.x'
+];
+
 describe('integration tests', () => {
   const eslint_d = path.resolve('bin/eslint_d.js');
   const require = createRequire(import.meta.url);
@@ -58,115 +67,113 @@ describe('integration tests', () => {
     });
   });
 
-  ['v4.0.x', 'v5.0.x', 'v6.0.x', 'v7.0.x', 'v8.0.x', 'v9.0.x'].forEach(
-    (fixture) => {
-      context(fixture, () => {
-        const cwd = path.resolve(`test/fixture/${fixture}`);
-        const { version: eslint_version } = require(
-          require.resolve('eslint/package.json', { paths: [cwd] })
-        );
-        const config = `${cwd}/node_modules/eslint/.eslint_d`;
-        let pid;
+  [SUPPORTED_ESLINT_VERSIONS].forEach((fixture) => {
+    context(fixture, () => {
+      const cwd = path.resolve(`test/fixture/${fixture}`);
+      const { version: eslint_version } = require(
+        require.resolve('eslint/package.json', { paths: [cwd] })
+      );
+      const config = `${cwd}/node_modules/eslint/.eslint_d`;
+      let pid;
 
-        after(async () => {
-          try {
-            await fs.unlink(config);
-          } catch {
-            // ignore
-          }
-        });
-
-        it('--version', async () => {
-          const { error, stdout, stderr } = await run('--version', { cwd });
-
-          assert.equals(
-            stdout,
-            `eslint_d: v${version}, bundled eslint: v${bundled_version}\n`
-          );
-          assert.equals(stderr, '');
-          assert.isNull(error);
-        });
-
-        it('status not running', async () => {
-          await assert.rejects(fs.stat(config));
-
-          const { error, stdout, stderr } = await run('status', { cwd });
-
-          assert.equals(
-            stdout,
-            `eslint_d: Not running - local eslint v${eslint_version}\n`
-          );
-          assert.equals(stderr, '');
-          assert.isNull(error);
-        });
-
-        it('start', async () => {
-          const { error, stdout, stderr } = await run('start', { cwd });
-
-          assert.equals(stdout, '');
-          assert.equals(stderr, '');
-          assert.isNull(error);
-          await assert.resolves(fs.stat(config));
-        });
-
-        it('status running', async () => {
-          const raw = await fs.readFile(config, 'utf8');
-          [, , pid] = raw.split(' ');
-
-          const { error, stdout, stderr } = await run('status', { cwd });
-
-          assert.equals(
-            stdout,
-            `eslint_d: Running (${pid}) - local eslint v${eslint_version}\n`
-          );
-          assert.equals(stderr, '');
-          assert.isNull(error);
-          refute.exception(() => process.kill(Number(pid), 0));
-        });
-
-        it('pass.js', async () => {
-          const { error, stdout, stderr } = await run('../pass.js', { cwd });
-
-          assert.equals(stdout, '');
-          assert.equals(stderr, '');
-          assert.isNull(error);
-        });
-
-        it('fail.js', async () => {
-          const { error, stdout, stderr } = await run('../fail.js', { cwd });
-
-          assert.match(stdout, '/test/fixture/fail.js');
-          assert.match(stdout, 'Strings must use singlequote');
-          refute.isNull(error);
-          assert.equals(error?.['code'], 1);
-          assert.equals(stderr, '');
-        });
-
-        it('--stdin', async () => {
-          const { error, stdout, stderr } = await run('--stdin', {
-            cwd,
-            stdin: `/* eslint quotes: ["error", "single"] */
-              console.log("hello");`
-          });
-
-          assert.match(stdout, '<text>');
-          assert.match(stdout, 'Strings must use singlequote');
-          assert.equals(stderr, '');
-          refute.isNull(error);
-          assert.equals(error?.['code'], 1);
-        });
-
-        it('stop', async () => {
-          const { error, stdout, stderr } = await run('stop', { cwd });
-
-          assert.equals(stdout, '');
-          assert.equals(stderr, '');
-          assert.isNull(error, stdout);
-          await assert.rejects(fs.stat(config));
-          await new Promise((resolve) => setTimeout(resolve, 50));
-          assert.exception(() => process.kill(Number(pid), 0));
-        });
+      after(async () => {
+        try {
+          await fs.unlink(config);
+        } catch {
+          // ignore
+        }
       });
-    }
-  );
+
+      it('--version', async () => {
+        const { error, stdout, stderr } = await run('--version', { cwd });
+
+        assert.equals(
+          stdout,
+          `eslint_d: v${version}, bundled eslint: v${bundled_version}\n`
+        );
+        assert.equals(stderr, '');
+        assert.isNull(error);
+      });
+
+      it('status not running', async () => {
+        await assert.rejects(fs.stat(config));
+
+        const { error, stdout, stderr } = await run('status', { cwd });
+
+        assert.equals(
+          stdout,
+          `eslint_d: Not running - local eslint v${eslint_version}\n`
+        );
+        assert.equals(stderr, '');
+        assert.isNull(error);
+      });
+
+      it('start', async () => {
+        const { error, stdout, stderr } = await run('start', { cwd });
+
+        assert.equals(stdout, '');
+        assert.equals(stderr, '');
+        assert.isNull(error);
+        await assert.resolves(fs.stat(config));
+      });
+
+      it('status running', async () => {
+        const raw = await fs.readFile(config, 'utf8');
+        [, , pid] = raw.split(' ');
+
+        const { error, stdout, stderr } = await run('status', { cwd });
+
+        assert.equals(
+          stdout,
+          `eslint_d: Running (${pid}) - local eslint v${eslint_version}\n`
+        );
+        assert.equals(stderr, '');
+        assert.isNull(error);
+        refute.exception(() => process.kill(Number(pid), 0));
+      });
+
+      it('pass.js', async () => {
+        const { error, stdout, stderr } = await run('../pass.js', { cwd });
+
+        assert.equals(stdout, '');
+        assert.equals(stderr, '');
+        assert.isNull(error);
+      });
+
+      it('fail.js', async () => {
+        const { error, stdout, stderr } = await run('../fail.js', { cwd });
+
+        assert.match(stdout, '/test/fixture/fail.js');
+        assert.match(stdout, 'Strings must use singlequote');
+        refute.isNull(error);
+        assert.equals(error?.['code'], 1);
+        assert.equals(stderr, '');
+      });
+
+      it('--stdin', async () => {
+        const { error, stdout, stderr } = await run('--stdin', {
+          cwd,
+          stdin: `/* eslint quotes: ["error", "single"] */
+              console.log("hello");`
+        });
+
+        assert.match(stdout, '<text>');
+        assert.match(stdout, 'Strings must use singlequote');
+        assert.equals(stderr, '');
+        refute.isNull(error);
+        assert.equals(error?.['code'], 1);
+      });
+
+      it('stop', async () => {
+        const { error, stdout, stderr } = await run('stop', { cwd });
+
+        assert.equals(stdout, '');
+        assert.equals(stderr, '');
+        assert.isNull(error, stdout);
+        await assert.rejects(fs.stat(config));
+        await new Promise((resolve) => setTimeout(resolve, 50));
+        assert.exception(() => process.kill(Number(pid), 0));
+      });
+    });
+  });
 });
