2892b45eeee234aa9aebcf3499ef13c36f11d272
Maximilian Antoni <mail@maxantoni.de>
2024-09-19 09:26:01 +0200

    Add coverage-reports to eslint and tsc ignores

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,6 @@ import studio_eslint_config from '@studio/eslint-config';
 export default [
   ...studio_eslint_config,
   {
-    ignores: ['bin/eslint.js', 'test/fixture/fail.js']
+    ignores: ['bin/eslint.js', 'test/fixture/fail.js', 'coverage-reports']
   }
 ];
